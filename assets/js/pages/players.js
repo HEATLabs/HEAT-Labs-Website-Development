730367f29a964801c1ba424d4718b8af45b6fb43
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-10-12 07:23:59 +0300

    added complete files for development version of the website

---
+++ assets/js/pages/players.js
@@ -0,0 +1,3 @@
+// PLACEHOLDER
+const PLAYERS_JSON_PATH = 'https://cdn.jsdelivr.net/gh/HEATLabs/HEAT-Labs-Database@main/mock-api/players.json';
+const CLANS_JSON_PATH = 'https://cdn.jsdelivr.net/gh/HEATLabs/HEAT-Labs-Database@main/mock-api/clans.json';
