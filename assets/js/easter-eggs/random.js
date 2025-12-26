43c4df6f47de5209ebb62a977f04ba596e6f97fa
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-12-27 00:06:35 +0200

    updated JS logic for random endpoint with random texts

--- assets/js/easter-eggs/random.js
+++ assets/js/easter-eggs/random.js
@@ -1,4 +1,30 @@
 (function() {
+    // Random texts
+    const randomTexts = [
+        "BEAM ME UP SCOTTY!",
+        "Taking you somewhere else, hold on..",
+        "Loading... please pretend this is impressive.",
+        "This won't take long. Probably.",
+        "Checking if this works.",
+        "Ah, you're finally awake",
+        "Hold on, I know a shortcut.",
+        "This seemed faster in testing.",
+        "Adjusting the timeline slightly.",
+        "Why are you here?",
+        "Remmy? is that you?",
+        "Searching for the holy grail.",
+        "And now for something completely different.",
+        "Putting far too much effort into what this is for."
+    ];
+
+    // Update the h1 tag
+    const h1 = document.querySelector('h1');
+    if (h1) {
+        const randomText = randomTexts[Math.floor(Math.random() * randomTexts.length)];
+        h1.textContent = randomText;
+    }
+
+    // Fetch and redirect
     fetch('https://raw.githubusercontent.com/HEATLabs/HEAT-Labs-Configs/refs/heads/main/search-keywords.json')
         .then(res => res.json())
         .then(data => {
