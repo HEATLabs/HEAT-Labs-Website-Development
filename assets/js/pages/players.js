ac59f1e7f9f271a5ff0873ccc9924e1c8db96ae0
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2026-08-11 22:15:29 +0300

    removed WIP player stats files

--- assets/js/pages/players.js
+++
@@ -1,3 +0,0 @@
-// PLACEHOLDER
-const PLAYERS_JSON_PATH = 'https://raw.githubusercontent.com/HEATLabs/HEAT-Labs-Images-Database/refs/heads/main/mock-api/players.json';
-const CLANS_JSON_PATH = 'https://raw.githubusercontent.com/HEATLabs/HEAT-Labs-Images-Database/refs/heads/main/mock-api/clans.json';
